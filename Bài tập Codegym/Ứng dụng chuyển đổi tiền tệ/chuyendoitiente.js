10d34fd655443c14f12b71cee0d74a937a014306
unknown <namnguyen73>
2023-02-09 10:40:18 +0700

    first commit

--- Bài tập Codegym/Ứng dụng chuyển đổi tiền tệ/chuyendoitiente.js
+++ Bài tập Codegym/Ứng dụng chuyển đổi tiền tệ/chuyendoitiente.js
@@ -1,21 +1,21 @@
 function ChangeMoney() {
-    let Amount = document.getElementById("Amount").value;
-    let FromC = document.getElementById("From").value;
-    let To = document.getElementById("To").value;
-    let Result;
+    let amount = document.getElementById("amount").value;
+    let fromC = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    let result;
 
 
-    if (FromC == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 23000) + " D";
+    if (fromC == "USD" && to == "VND") {
+        result = "result: " + (amount * 23000) + " D";
     }
-    else if (FromC == "VND"&& To =="USD"){
-        Result = "Result: "+ (Amount / 23000) + " $";
+    else if (fromC == "VND"&& to =="USD"){
+        result = "result: "+ (amount / 23000) + " $";
     }
-    else if (FromC == "VND") {
-        Result = "Result: " + Amount + " D"
+    else if (fromC == "VND") {
+        result = "result: " + amount + " D"
     } else {
-        Result = "Result: " + Amount + " $"
+         result = "result: " + amount + " $"
     }
 
-    document.getElementById("Result").innerHTML = Result;
+    document.getElementById("result").innerHTML = result;
 }
